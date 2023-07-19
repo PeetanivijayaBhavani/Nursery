8af03b5ac622232a55600a6902fa943d75273a7c
PeetaniVijayaBhavani <125951404+PeetanivijayaBhavani@users.noreply.github.com>
2023-07-19 11:03:50 +0530

    Add files via upload

---
+++ flowerscript.js
@@ -0,0 +1,12 @@
+function startingstage()
+{
+    document.getElementById("flower").src="https://hips.hearstapps.com/clv.h-cdn.co/assets/15/10/1425667253-nursery-shopping.jpg";
+}
+function middlestage()
+{
+    document.getElementById("flower").src="https://content.jdmagicbox.com/comp/def_content/plant_nurseries/default-plant-nurseries-9.jpg";
+}
+function endingstage()
+{
+    document.getElementById("flower").src="https://hgtvhome.sndimg.com/content/dam/images/grdn/fullset/2014/2/26/0/img-9193.JPG.rend.hgtvcom.966.725.suffix/1452659786303.jpeg";
+}
